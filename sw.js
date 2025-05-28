ceb6dffd2e780d61ee3d6551df5e6b41a1557b98
dmitr <dmitr@COMP-PC5348.compute.dtu.dk>
2025-05-28 10:50:31 +0200

    good

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './simulator.js',
-  './simulator.wasm',
+  './simulator-250da25e97e8316b_bg.wasm',
+  './simulator-250da25e97e8316b.js',
 ];
 
 /* Start the service worker and cache all of the app's content */
